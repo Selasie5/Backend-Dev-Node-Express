bc1e00d45aa21db550696db321085485e255c548
Selasie5 <selasisepenu5@gmail.com>
2023-12-14 11:21:31 +0000

    Created an env file and accesse it in my entry point file

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const req = require("express/lib/request");
 const app = express();
-const port = 8080
+const port = 3000
 
 //serving a specific json file thorugh a specific route
 app.get("/json", (req,res)=>{
-    res.json({
+    const messageStyle = process.env.MESSAGE_STYLE;
+    const jsonResponse = 
+    {
         "id":"1",
         "productName": "Logitech Mechanical Keyboard",
         "productPrice": "$230"
-        
-    })
+    }
+    const transformedResponse =transformedKeys(jsonResponse)
+    res.json(transformedResponse)
+   function transformedKeys(obj, style)
+   {
+    const transformedObj={};
+    for (let key in obj)
+    {
+        if(Object.prototype.hasOwnProperty.call(obj,key))
+        {
+            transformedObj[style === "uppercase"? key.toUpperCase(): key.toLowerCase()]= obj[key];
+        }
+    }
+    return transformedObj;
+   } 
 })
 app.listen(port, (()=>
 {
